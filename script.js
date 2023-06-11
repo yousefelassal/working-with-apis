cf5a34c3af59533bafc26335c2a8f6ca3372f192
Yousef <yousefsameh24@gmail.com>
2023-06-11 19:35:45 +0300

    handle btn clicks

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const img = document.querySelector('img');
+const button = document.querySelector('button');
 
-fetch(`https://api.giphy.com/v1/gifs/translate?api_key=0m9yqmprOR9tjDYccGrRF8ioYlVBmD0f&s=cats`, {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        img.src = response.data.images.original.url;
-    });
+
+function getGiphy() {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=0m9yqmprOR9tjDYccGrRF8ioYlVBmD0f&s=cats`, {mode: 'cors'})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            img.src = response.data.images.original.url;
+        });
+};
+
+getGiphy();
+
+button.addEventListener('click', () => {
+    getGiphy();
+});
+
