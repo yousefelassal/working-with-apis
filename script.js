e582954bce58168eda6aaaccc8b3d200179ad97f
Yousef <yousefsameh24@gmail.com>
2023-06-11 19:05:44 +0300

    add skeleton

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const img = document.querySelector('img');
+
 fetch(`https://api.giphy.com/v1/gifs/translate?api_key=0m9yqmprOR9tjDYccGrRF8ioYlVBmD0f&s=cats`, {mode: 'cors'})
     .then(function(response) {
         return response.json();
